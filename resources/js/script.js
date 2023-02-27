ee8002e83c36f27fd75f3093e8e1c5925fb11ab7
Kevin Wenzel <kevinmwenzel@gmail.com>
2023-02-27 09:49:21 -0500

    Change hamburger dropdown to JS

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,3 +2,8 @@ function darkMode() {
     const element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function dropDown() {
+    const menu = document.getElementById("dropdown");
+    menu.classList.toggle("show");
+}
